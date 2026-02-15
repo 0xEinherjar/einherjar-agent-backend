206d7103602906f14064ecaa2415e7591261dbe7
Einherjar <rodrigo.silva1186@etec.sp.gov.br>
2026-02-15 18:59:01 -0300

    update

--- src/middleware/security.js
+++ src/middleware/security.js
@@ -36,6 +36,5 @@ export const apiRateLimiter = rateLimit({
 export const corsOptions = {
   origin: true,
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
 };
